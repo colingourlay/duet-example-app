aaac11225d585f603346fbd6b4098d7acd6e5930
Colin Gourlay <colin@colin-gourlay.com>
2016-01-31 17:50:16 +1000

    var->const

--- src/model.js
+++ src/model.js
@@ -1,7 +1,7 @@
 const {model, value, localStorage} = require('duet');
 
 module.exports = (initialState) => {
-    var count = value(initialState != null ? parseInt(initialState, 10) : 0);
+    const count = value(initialState != null ? parseInt(initialState, 10) : 0);
 
     count(function (state) {
         localStorage('count', state);
